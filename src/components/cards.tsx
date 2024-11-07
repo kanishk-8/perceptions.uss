8a0fba01536a8f5d9fd72712d8892fc4bd08fa53
kanishk-8 <kanishkkumar222004@gmail.com>
2024-11-07 16:25:17 +0530

    added responsiveness

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -2,9 +2,7 @@ import React from "react";
 
 function Card() {
   return (
-    <div className=" bg-[#292A2C] w-1/6 h-36 rounded-xl text-primarytext">
-      cards
-    </div>
+    <div className=" bg-[#292A2C] mx-1 w-full min-h-[5rem] md:min-h-[15rem] md:mx-2 rounded-xl text-primarytext"></div>
   );
 }
 
