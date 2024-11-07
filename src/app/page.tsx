85bb5446f745fa7e131b0a376303b297f91f8109
kanishk-8 <kanishkkumar222004@gmail.com>
2024-11-07 13:35:06 +0530

    footer navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <></>
-  );
+  return <div className="h-screen bg-primarycolor">hello</div>;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primarycolor: "#151617",
+        primarytext: "#E9ECEF",
       },
     },
   },
